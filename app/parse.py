316f9abcfacc8b5a1f213565659987a5c407e869
benfict <benfict@gmail.com>
2025-10-21 02:40:35 +0300

    Solution

--- app/parse.py
+++ app/parse.py
@@ -1,5 +1,11 @@
-from dataclasses import dataclass
+import csv
+from dataclasses import dataclass, fields, astuple
 
+import requests
+from bs4 import BeautifulSoup, Tag
+
+
+BASE_URL= "https://quotes.toscrape.com/"
 
 @dataclass
 class Quote:
@@ -7,10 +13,48 @@ class Quote:
     author: str
     tags: list[str]
 
+QUOTE_FIELDS = [field.name for field in fields(Quote)]
+
+def parse_quotes(quote: Tag) -> Quote:
+    return Quote(
+        text=quote.select_one(".text").text,
+        author=quote.select_one(".author").text,
+        tags=[tag.text for tag in quote.select(".tag")]
+    )
+
+def get_all_quotes() -> list[Quote]:
+    all_quotes: list[Quote] = []
+    url = BASE_URL
+    page = 1
+
+    while url:
+        response = requests.get(url)
+        response.raise_for_status()
+        soup = BeautifulSoup(response.text, "html.parser")
+
+        all_quotes.extend([parse_quotes(q) for q in soup.select(".quote")])
+
+        next_link = soup.select_one(".pager .next a")
+        if next_link:
+            url = BASE_URL.rstrip("/") + next_link["href"]
+            page += 1
+        else:
+            url = None
+
+    return all_quotes
+
+def write_quotes_to_csv(quotes: [Quote], output_csv_path: str ) -> None:
+    with open(output_csv_path, "w") as f:
+        writer = csv.writer(f)
+        writer.writerow(QUOTE_FIELDS)
+        writer.writerows([astuple(quote) for quote in quotes])
+
 
 def main(output_csv_path: str) -> None:
-    pass
+    quotes = get_all_quotes()
+    write_quotes_to_csv(quotes, output_csv_path)
 
 
 if __name__ == "__main__":
     main("quotes.csv")
+
